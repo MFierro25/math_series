c891494681adee922bbc179652ba94df58b014dd
Miguel Fierro <emfierro2@gmail.com>
2021-11-30 21:58:03 -0800

    created math series and working sum series function with tests

--- math_series/series.py
+++ math_series/series.py
@@ -1,13 +1,26 @@
 def fibonacci(n):
+    '''this function recreates the fibonacci math equation
+    it is a series of numbers which each number is the sum of the
+    two preceding numbers'''
     if n <= 1:
         return n
     else:
         return fibonacci(n - 1) + fibonacci(n -2)
 
 def lucas(n):
-
+    '''this function recreates lucas numbers, closely related to fibonacci'''
     if n == 0:
         return 2
     if n == 1:
         return 1
     return lucas(n - 1) + lucas(n - 2)
+
+def sum_series(n, first = 0, second = 1):
+    '''returns the nth number in the series created by summing previous two numbers.
+    also accepts two parameters representing first two numbers in the series.'''
+    if n == 0:
+        return first
+    if n == 1:
+        return second
+    else:
+        return sum_series(n - 1, first, second) + sum_series(n -2, first, second)
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,6 @@
 from math_series.series import fibonacci
 from math_series.series import lucas
+from math_series.series import sum_series
 import pytest
 
 def test_import():
@@ -46,4 +47,14 @@ def test_lucas_one():
 def test_lucas_seven():
     actual = lucas(7)
     expected = 29
+    assert actual == expected
+    
+def test_sum_series_zero():
+    actual = sum_series(0)
+    expected = fibonacci(0)
+    assert actual == expected
+    
+def test_sum_series_one():
+    actual = sum_series(1)
+    expected = fibonacci(1)
     assert actual == expected
